90075b68efbe1b245fec6121097518747cc2451c
0xvikasrushi <0xvikas@gmail.com>
2025-06-10 20:55:52 +0530

    fix: circomkit version

--- wallet-unit-poc/packages/jwt-es256-circom/tests/claim-decoder.test.ts
+++ wallet-unit-poc/packages/jwt-es256-circom/tests/claim-decoder.test.ts
@@ -28,7 +28,7 @@ describe("ClaimDecoder", () => {
 
     const { claimArray, claimLengths } = encodeClaims(inputs, maxClaims, maxClaimsLength);
 
-    const decodeFlags = [0, 0, 0];
+    const decodeFlags = [1, 1, 1];
     const witness = await circuit.calculateWitness({
       claims: claimArray,
       claimLengths,
@@ -40,27 +40,85 @@ describe("ClaimDecoder", () => {
     const decodedClaims = outputs.decodedClaims as number[][];
     const circuitClaimHash = outputs.claimHashes as number[][];
 
-    console.log("Decoded claims:", decodedClaims);
-    console.log("Claim hashes:", circuitClaimHash);
+    for (let i = 0; i < inputs.length; i++) {
+      const length = Number(claimLengths[i]);
+      const base64 = decodedClaims[i]
+        .slice(0, length)
+        .map((c) => String.fromCharCode(Number(c)))
+        .join("")
+        .replace(/[\x00-\x1F]+$/g, "");
 
-    
+      assert.strictEqual(base64, expectedOutputs[i]);
 
-    // for (let i = 0; i < inputs.length; i++) {
-    //   const length = Number(claimLengths[i]);
-    //   const base64 = decodedClaims[i]
-    //     .slice(0, length)
-    //     .map((c) => String.fromCharCode(Number(c)))
-    //     .join("")
-    //     .replace(/[\x00-\x1F]+$/g, "");
+      const expectedHash = sha256(Uint8Array.from(Buffer.from(inputs[i].slice(0, length), "utf8")));
+      const expectedHashHex = Array.from(expectedHash, (b) => b.toString(16).padStart(2, "0")).join("");
+      const circuitHashHex = circuitClaimHash[i].map((b) => b.toString(16).padStart(2, "0")).join("");
 
-    //   assert.strictEqual(base64, expectedOutputs[i]);
+      assert.strictEqual(circuitHashHex, expectedHashHex);
+    }
+    await circuit.expectConstraintPass(witness);
+  });
 
-    //   const expectedHash = sha256(Uint8Array.from(Buffer.from(inputs[i].slice(0, length), "utf8")));
-    //   const expectedHashHex = Array.from(expectedHash, (b) => b.toString(16).padStart(2, "0")).join("");
-    //   const circuitHashHex = circuitClaimHash[i].map((b) => b.toString(16).padStart(2, "0")).join("");
+  it("It should decode raw claims without padding correctly", async () => {
+    const testcase = [
+      "WyJmSGlPTE9ZRVFhZkF3MjBCZjRxZXpBIiwibmFtZSIsIumZs-etseeOsiJd",
+      "WyJLVXYxVF9BNXpvVDlJbXFURmUwdUxnIiwiaWRfbnVtYmVyIiwiQTIzNDU2Nzg5MCJd",
+      "WyJuTDVDa2VaV2paSG13UjcxV05lWlZ3Iiwicm9jX2JpcnRoZGF5IiwiMDU3MDYwNSJd",
+      "WyJvZFNweWFjaUNuZUJneld1VEFyM0pRIiwidHlwZSIsIuaZrumAmuWwj-Wei-i7iiJd",
+      "WyJJdFVGQUV2S0kybFJCV2MzU19LTjhnIiwiY29udHJvbG51bWJlciIsIjQwMTA0MDIwOTE0NDUiXQ",
+      "WyJROWEySWM3b1IxUjRFQ0VXX3RYaUlRIiwiZ0RhdGUiLCIxMDIwNzAxIl0",
+    ];
+    const maxClaimsLength = 128;
+    const maxClaims = 6;
 
-    //   assert.strictEqual(circuitHashHex, expectedHashHex);
-    // }
-    // await circuit.expectConstraintPass(witness);
+    circuit = await circomkit.WitnessTester("ClaimDecoder", {
+      file: "claim-decoder",
+      template: "ClaimDecoder",
+      params: [maxClaims, maxClaimsLength],
+      recompile: true,
+    });
+
+    const decodeFlags = [0, 0, 1, 0, 0, 0];
+    const base64urlToBase64 = (b64url: string) => {
+      let b64 = b64url.replace(/-/g, "+").replace(/_/g, "/");
+      const pad = (4 - (b64.length % 4)) % 4;
+      return b64 + "=".repeat(pad);
+    };
+    const expectedOutputs = testcase.map((s) => atob(base64urlToBase64(s)));
+    const { claimArray, claimLengths } = encodeClaims(testcase, maxClaims, maxClaimsLength);
+
+    const witness = await circuit.calculateWitness({
+      claims: claimArray,
+      claimLengths,
+      decodeFlags,
+    });
+
+    const outputs = await circuit.readWitnessSignals(witness, ["decodedClaims", "claimHashes"]);
+
+    const decodedClaims = outputs.decodedClaims as number[][];
+    const circuitClaimHash = outputs.claimHashes as number[][];
+
+    for (let i = 0; i < testcase.length; i++) {
+      const length = Number(claimLengths[i]);
+      const base64 = decodedClaims[i]
+        .slice(0, length)
+        .map((c) => String.fromCharCode(Number(c)))
+        .join("")
+        .replace(/[\x00-\x1F]+$/g, "");
+
+      if (decodeFlags[i] === 1) {
+        assert.strictEqual(base64, expectedOutputs[i]);
+      } else {
+        assert.strictEqual(base64, "");
+      }
+
+      const expectedHash = sha256(Uint8Array.from(Buffer.from(testcase[i].slice(0, length), "utf8")));
+      const expectedHashHex = Array.from(expectedHash, (b) => b.toString(16).padStart(2, "0")).join("");
+      const circuitHashHex = circuitClaimHash[i].map((b) => b.toString(16).padStart(2, "0")).join("");
+
+      assert.strictEqual(circuitHashHex, expectedHashHex);
+    }
+
+    await circuit.expectConstraintPass(witness);
   });
 });
